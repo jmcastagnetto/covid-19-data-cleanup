3e2c54667127cc7a14de851ec0063fa1d3b59088
Jesus M. Castagnetto <jesus@castagnetto.com>
2020-02-11 09:02:06 -0500

    combine timeseries, make them tsibbles

--- 02-process-timeseries.R
+++ 02-process-timeseries.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(gh)
 
+#-- get the list of CSV files with the timeseries
 tsfiles <- gh("GET /repos/:owner/:repo/contents/:path",
             owner = "CSSEGISandData",
             repo = "2019-nCoV",
@@ -29,59 +30,74 @@ tsfiles_df <- tsfiles %>%
     str_detect(name, "\\.csv")
   )
 
-ts_confirmed <- read_csv(
-  tsfiles_df[1,]$download_url,
-  col_types = cols(.default = col_character())
-  ) %>%
-  pivot_longer(
-    -c("Province/State", "Country/Region", "Lat", "Long"),
-    names_to = "ts",
-    values_to = "confirmed"
-  ) %>%
-  mutate(
-    ts = lubridate::mdy_hm(ts),
-    confirmed = as.integer(confirmed)
-  ) %>%
-  janitor::clean_names() %>%
-  select(2, 1, 3, 4, 5, 6)
+#-- utility functions
 
-ts_deaths <- read_csv(
-  tsfiles_df[2,]$download_url,
-  col_types = cols(.default = col_character())
-  ) %>%
-  pivot_longer(
-    -c("Province/State", "Country/Region", "Lat", "Long"),
-    names_to = "ts",
-    values_to = "deaths"
-  ) %>%
-  mutate(
-    ts = lubridate::mdy_hm(ts),
-    deaths = as.integer(deaths)
-  ) %>%
-  janitor::clean_names() %>%
-  select(2, 1, 3, 4, 5, 6)
+get_tsdata <- function(csv_url) {
+  read_csv(csv_url,
+           col_types = cols(.default = col_character())) %>%
+    pivot_longer(
+      -c("Province/State", "Country/Region", "Lat", "Long"),
+      names_to = "ts",
+      values_to = "confirmed"
+    ) %>%
+    janitor::clean_names() %>%
+    mutate(
+      ts = lubridate::mdy_hm(ts),
+      confirmed = as.integer(confirmed),
+      lat = round(as.double(lat), 5),
+      long = round(as.double(long), 5)
+    ) %>%
+    select(2, 1, 3, 4, 5, 6)
+}
 
-ts_recovered <- read_csv(
-  tsfiles_df[3,]$download_url,
-  col_types = cols(.default = col_character())
-  ) %>%
-  pivot_longer(
-    -c("Province/State", "Country/Region", "Lat", "Long"),
-    names_to = "ts",
-    values_to = "recovered"
-  ) %>%
-  mutate(
-    ts = lubridate::mdy_hm(ts),
-    recovered = as.integer(recovered)
-  ) %>%
-  janitor::clean_names() %>%
-  select(2, 1, 3, 4, 5, 6)
+mk_tsibble <- function(df) {
+  tsibble::as_tsibble(
+    df,
+    key = c("country_region", "province_state", "lat", "long"),
+    index = ts
+  )
+}
 
+#-- get the data
+ts_confirmed <- get_tsdata(tsfiles_df[1,]$download_url)
+ts_deaths <- get_tsdata(tsfiles_df[2,]$download_url)
+ts_recovered <- get_tsdata(tsfiles_df[3,]$download_url)
+
+#-- extract places
+places <- bind_rows(
+  ts_confirmed %>% select(1,2,3,4),
+  ts_deaths %>% select(1,2,3,4),
+  ts_recovered %>% select(1,2,3,4)
+) %>%
+  distinct() %>%
+  arrange(country_region, province_state)
+
+#-- combine all data and make them tsibbles
+ts_combined <- as_tibble(ts_confirmed) %>%
+  select(-lat, -long) %>%
+  left_join(
+    ts_deaths %>% select(-lat, -long),
+    by = c("country_region", "province_state", "ts")
+  ) %>%
+  left_join(
+    ts_recovered %>% select(-lat, -long),
+    by = c("country_region", "province_state", "ts")
+  ) %>%
+  left_join(
+    places,
+    by = c("country_region", "province_state")
+  ) %>%
+  mk_tsibble()
+
+ts_confirmed <- mk_tsibble(ts_confirmed)
+ts_deaths <- mk_tsibble(ts_deaths)
+ts_recovered <- mk_tsibble(ts_recovered)
+
+#-- save timeseries
 saveRDS(
   ts_confirmed,
   file = "data/2019ncov_ts_confirmed.rds"
 )
-
 
 saveRDS(
   ts_deaths,
@@ -93,7 +109,10 @@ saveRDS(
   file = "data/2019ncov_ts_recovered.rds"
 )
 
-
+saveRDS(
+  ts_combined,
+  file = "data/2019ncov_ts_combined.rds"
+)
 
 
 
